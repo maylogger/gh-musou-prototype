90eac53beb5c57179851610f885092500d02d0f8
Tou Neko <touneko@gmail.com>
2013-12-24 22:38:13 +0800

    off canvas menu button & collapse button change text js

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,3 +1,13 @@
+// off canvas menu switch
 $(".menu-btn").click(function(){
   $("body").toggleClass("open-menu");
+});
+
+//collapse button switch content
+$(".collapse-toggle-button").click(function(){
+	if($(this).text() == '看更多'){
+           $(this).text('看三小');
+       } else {
+           $(this).text('看更多');
+       }
 });
