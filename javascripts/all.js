2a77c7029e97cfcd364ff00917c95800cbb651b1
Tou Neko <touneko@gmail.com>
2014-01-03 19:48:27 +0800

    戰報頁面

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,13 +1,20 @@
+$(function(){
+	$(".versus").addClass("animate");
+});
 // off canvas menu switch
 $(".menu-btn").click(function(){
   $("body").toggleClass("open-menu");
 });
 
 //collapse button switch content
+// $(".collapse-toggle-button").click(function(){
+// 	if($(this).text() == '看更多'){
+//            $(this).text('收回去');
+//        } else {
+//            $(this).text('看更多');
+//        }
+// });
+
 $(".collapse-toggle-button").click(function(){
-	if($(this).text() == '看更多'){
-           $(this).text('收回去');
-       } else {
-           $(this).text('看更多');
-       }
+	$(this).remove();
 });
